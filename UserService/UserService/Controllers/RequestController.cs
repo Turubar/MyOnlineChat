93fbae296848bc6caf891d77f293913679aa300b
Ivan <ivan.turubar@gmail.com>
2024-07-01 23:56:04 +0300

    Настройка проекта

---
+++ UserService/UserService/Controllers/RequestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserService.Models;
+using UserService.Models.Entities;
+
+namespace UserService.Controllers
+{
+    [Route("api")]
+    public class RequestController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public RequestController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [Route("users")]
+        public DbSet<User> GetUsers ()
+        {
+            return _context.Users;
+        }
+    }
+}
---
+++ UserService/UserService/Models/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace UserService.Models.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Nickname { get; set; }
+    }
+}
